7c7e10a1a7102d1a03a0fb84088d49ddf2362b6e
Mr. Tasmir Khan <masteroffcodes@gmail.com>
2024-12-04 07:18:11 +0000

    first commit

---
+++ DSA Codes/first.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class first{
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       int a = sc.nextInt(); int b=0;
+       
+       while(a>0){
+         a = a/10;
+         b++;
+       }
+      System.out.println("No. of digits in the given value is "+b);
+      
+    }
+}
