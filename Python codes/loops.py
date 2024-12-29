c16f9bf4f6b5e64855113fe95220dd3c5e611527
Mr. Tasmir Khan <masteroffcodes@gmail.com>
2024-12-29 06:11:39 +0000

    updated code for loops using range and pass

--- Python codes/loops.py
+++ Python codes/loops.py
@@ -1,32 +1,48 @@
-print("In this program we are going to use loops")
-i=1
-while i<=100:
-    print(i)
-    i+=1
-print("\ndone")
-i-=1
-while i>=1:
-    print(i)
-    i-=1
-print("\ndone")
+# print("In this program we are going to use loops")
+# i=1
+# while i<=100:
+#     print(i)
+#     i+=1
+# print("\ndone")
+# i-=1
+# while i>=1:
+#     print(i)
+#     i-=1
+# print("\ndone")
 
-x = int(input("Enter a number "))
-j=1
-while j<=10:
-    print(x,"X",j,"=",x*j )
-    j+=1
+# x = int(input("Enter a number "))
+# j=1
+# while j<=10:
+#     print(x,"X",j,"=",x*j )
+#     j+=1
 
-list = [1,4,9,16,25,36,49,64,81,100]
-i = 0
-while i<len(list):
-    print(list[i])
-    i+=1
+# list = [1,4,9,16,25,36,49,64,81,100]
+# i = 0
+# while i<len(list):
+#     print(list[i])
+#     i+=1
 
-tuple = (1,4,9,16,25,36,49,64,81,100)
-input = int(input("enter the no. to be searched "))
-i=0
-while i<len(tuple):
-   if tuple[i] == input:
-    print("Index of the given no. is",i)
+# tuple = (1,4,9,16,25,36,49,64,81,100)
+# input = int(input("enter the no. to be searched "))
+# i=0
+# while i<len(tuple):
+#    if tuple[i] == input:
+#     print("Index of the given no. is",i)
 
-   i+=1
+#    i+=1
+
+# print("Program for range")
+# no = int(input("enter the number"))
+# for el in range(1,11):
+#     print(el,"X",no,"=",el*no)
+
+print("\nProgram to find sum of first n numbers")
+x = int(input("Enter the no."))
+a=0
+temp=0
+# for el in range(x+1):
+#     a = a+el
+while a<=x:
+    temp = temp+a
+    a+=1
+print(temp)
