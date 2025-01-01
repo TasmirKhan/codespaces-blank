ea59305f1aee3f25c779949dd483b459b3461ee3
Mr. Tasmir Khan <masteroffcodes@gmail.com>
2025-01-01 07:42:58 +0000

    Updated the folder and files

--- Python codes/fileio.py
+++ Python codes/fileio.py
@@ -1,5 +1,24 @@
 print("Program to show file input output")
-f = open("demo.txt","r")
-f2 = open("practice2.txt","w")
-print(f.read())
-f.close()
+with open("practice.txt", "w+") as f:
+    f.write("Hi everyone \nWe are learning file I/O \nUsing Java.\nI like programming in java")
+    
+# with open("practice.txt", "r") as f2:
+#     print(f2.read())
+#     f2.seek(0)
+#     print(f2.read(5))
+#     f2.seek(0)
+#     print(f2.readline())
+#     f2.seek(0)
+#     print(f2.readlines())
+    
+with open("practice.txt", "r") as f:
+    content = f.read()
+    print(content)
+    print(content.split())
+    content = content.replace("java","Python").replace("Java", "Python")
+    print(content)
+    if "learning" in content:
+        print("Yes")
+    else:
+        print("No")
+
