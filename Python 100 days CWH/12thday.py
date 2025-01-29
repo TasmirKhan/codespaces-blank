7048b3833e90174678a9fb5b49ea0c55def6c6e5
Mr. Tasmir Khan <masteroffcodes@gmail.com>
2025-01-29 14:07:37 +0000

    Methods of String

---
+++ Python 100 days CWH/12thday.py
@@ -0,0 +1,7 @@
+print("Here we are going to use methods in the String.")
+s = input("Enter the string: ")
+print("Getting Character from the String ",s[0] )
+print("Length of the String",len(s))
+nm = "Harry"
+print(nm[1:5])
+print(nm[-4:-2])
