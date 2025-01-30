054062067b3f52440110c95308a951565604af72
Mr. Tasmir Khan <masteroffcodes@gmail.com>
2025-01-30 14:47:02 +0000

    Exercise 2 creating program to

--- Python 100 days CWH/15thdayExercise.py
+++ Python 100 days CWH/15thdayExercise.py
@@ -1,7 +1,9 @@
 print("Create a python program capable of greeting you with Good Morning, Good Afternoon and Good Evening. Your program should use time module to get the current hour. Here is a sample program")
 import time
 timestamp = time.strftime('%H:%M:%S')
+timestampp = time.strftime('%H%M%S')
 print(timestamp)
+print(timestampp)
 # timestamp = time.strftime('%H')
 # print(timestamp)
 # timestamp = time.strftime('%M')
@@ -11,3 +13,18 @@ print(timestamp)
 
 current_Hour = time.localtime().tm_hour
 print(current_Hour)
+# Good Morning → 05:00 - 11:59
+# Good Afternoon → 12:00 - 17:59
+# Good Evening → 18:00 - 20:59
+# Good Night → 21:00 - 04:59
+rt = int(current_Hour)
+if(rt>=5 and rt<12):
+    print("Good Morning")
+elif(rt>=12 and rt<18):
+    print("Good AfterNoon")
+elif(rt>=18 and rt<21):
+    print("Good evening")
+elif(rt>=21 or rt<5):
+    print("Good Night")
+
+print("Thank You!")
